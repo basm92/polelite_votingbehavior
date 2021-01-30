59db0aa8091b972c4e7d6f7856c7569465c649ce
basm92 <basmachielsen@live.nl>
2021-01-30 21:44:38 +0100

    add a bunch of important code and edit the data file a bit

--- code/code_for_figures/figure_map_amountoftimesvote.R
+++ code/code_for_figures/figure_map_amountoftimesvote.R
@@ -1,7 +1,20 @@
 # maak een kaart van districten en willingness to accept fiscal laws 
+# of course, this can only be done for Tweede Kamer
+library(readr)
+library(readxl)
+library(tidyverse)
+library(stringr)
+library(stringdist)
+
+source("./code/helpers/find_district_acode.R")
 
 ## find the districts of the politicians
+df <- read_csv("./data/voting_behavior/votingbehavior_together.csv") %>%
+    filter(house == "Tweede Kamer")
 
+df %>%
+    group_split(law) %>%
+    lapply(find_district)
 
 ## find the map on municipality level
 
