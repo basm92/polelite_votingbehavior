c7f0f52b8764cb40f1e32068214997f777443b03
basm92 <basmachielsen@live.nl>
2021-02-07 11:47:58 +0100

    electoarl law results

--- code/analysis/electorallaw_results.R
+++ code/analysis/electorallaw_results.R
@@ -1,4 +1,10 @@
 #electorallaw_results
+library(stargazer)
+library(tidyverse)
+library(readxl)
+library(stringr)
+
+
 source("./code/data_treatment/get_data_from_voting_records_df.R")
 source("./code/data_treatment/get_polid_tk_and_get_polid_ek.R")
 files <- list.files("./data/voting_behavior/lowerhouse/electoral_law/")
